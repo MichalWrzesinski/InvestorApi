d8948b7599c685b0f08fb66efd69c3767f22df38
MichalWrzesinski <kontakt@webhome.pl>
2025-04-01 15:13:56 +0200

    Update rules PHP CS fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,26 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
-;
+    ->exclude('var');
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+        ],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+        ],
+        'phpdoc_order' => true,
+        'no_unused_imports' => true,
+        'fully_qualified_strict_types' => true,
+        'no_superfluous_phpdoc_tags' => true,
+        'phpdoc_align' => ['align' => 'vertical'],
+        'yoda_style' => true,
+        'single_line_throw' => true,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
